229e3fe29e6cf7bd5b5b39e1da8b48000c1b7ad6
Daniel Wu <dw352934@gmail.com>
2017-09-17 18:49:32 -0400

    Bare skeleton of hrm made to edit

--- hrm.py
+++ hrm.py
@@ -1,1 +1,20 @@
 #HRM
+
+import pytest
+import pytest_pep8
+import numpy
+
+def peakDetector(ecg_data):
+    """Insert function here"""
+
+def instant(time, peak):
+    """Insert function here"""
+
+def average(time, peak):
+    """Insert function here"""
+
+def anomaly(time, peak):
+    """Insert function here"""
+
+def main(ecg_data, user_specified_time = 2000, brady_threshold = 50, tachy_threshold = 100):
+    """Insert function here"""
