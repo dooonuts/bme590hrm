c81e60bc8a51833434cbf99fd980c6b9a0b303d1
Daniel Wu <dw352934@gmail.com>
2017-09-19 09:41:20 -0400

    Pace Detection Works Properly, awaiting further insturctions

--- hrm.py
+++ hrm.py
@@ -14,24 +14,25 @@ def peakDetector(ecg_data):
         for row in heartreader:
             voltageList=list(row["voltage"])"""
 
-    data = pandas.read_csv(ecg_data, converters = {"times":float,"voltage":float}, names = {"times2","voltage2"})
-    print(data.times2)
-    #avgVoltage = numpy.mean()
-    #print(avgVoltage)
+    data = pandas.read_csv(ecg_data, converters = {"times":float,"voltage":float})
+    avgVoltage = numpy.mean(data.voltage.values)
+    minVoltage = numpy.min(data.voltage.values)
+    threshVoltage = avgVoltage + minVoltage
+
+    threshTimes= numpy.where(data.voltage.values>threshVoltage)
+    print(threshTimes)
 
 def instant(time, targetTime=0):
     """Insert function here"""
     if targetTime > time[len(time)-1]:
-        raise "Target time is out of range of detected peaks", targetTime
+        raise ("Target time is out of range of detected peaks", targetTime)
 
-    for x in range(0,len(time))
+    for x in range(0,len(time)):
         if time[x] >= targetTime:
             if x+1 >= len(time):
-                raise "Target time is out of range of detected peaks", targetTime
+                raise ("Target time is out of range of detected peaks", targetTime)
             instant_dt = time[x+1] - time[x]
     return 1/instant_dt
-
-
 
 def average(time, begin_time, end_time):
     """Insert function here"""
@@ -62,9 +63,6 @@ def average(time, begin_time, end_time):
     time_avg = time_count/div
 
     return 1/time_avg
-
-    
-
 
 def anomaly(time, peak):
     """Insert function here"""
