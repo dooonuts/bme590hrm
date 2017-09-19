2f76a4223b92c118bb8168789fb44735f38acc61
cshackerpro <brianna.loomis@duke.edu>
2017-09-18 22:18:39 -0400

    Added the average function to hrm

--- hrm.py
+++ hrm.py
@@ -13,8 +13,38 @@ def peakDetector(ecg_data):
 def instant(time, peak):
     """Insert function here"""
 
-def average(time, peak):
+def average(time, begin_time, end_time):
     """Insert function here"""
+    if time[len(time)] < end_time:
+        raise ValueError('End time occurs outside of range of csv file')
+    if time[len(time)] < begin_time:
+        raise ValueError('Begin time occurs outside of range of csv file')
+
+    begin = 0
+    end   = 0
+
+    for i in range(1, len(time)):
+        if time[i-1] == begin_time:
+            begin = i-1
+        elif (time[i-1] < begin_time and time[i] > begin_time):
+            begin = i
+        if time[i-1] == end_time:
+            end = i-1
+        elif (time[i-1] < end_time and time[i] > end_time):
+            end = i
+    
+    time_count = 0
+
+    for k in range(begin+1, end+1):
+        time_count = time[k-1] - time[k]
+    
+    div = begin-end
+    time_avg = time_count/div
+
+    return 1/time_avg
+
+    
+
 
 def anomaly(time, peak):
     """Insert function here"""
