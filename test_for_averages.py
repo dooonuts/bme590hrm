03c6dc7abece1bdff5893e60e524b9ccfbe3f8f8
syiyuli <samuel.li@duke.edu>
2017-09-25 18:58:20 -0400

    fixing test second scaling

--- test_for_averages.py
+++ test_for_averages.py
@@ -12,7 +12,7 @@ def test_average():
     assert hrm.main('full_test.csv', avg = True)==80;
 
     # Test for range 
-    assert hrm.main('full_test.csv', 30000, 45000, avg = True)==50;
+    assert hrm.main('full_test.csv', 30 45, avg = True)==50;
 
 def test_outofrange_average():
     """ Unit test for checking ValueErrors in avg fun of hrm
@@ -23,11 +23,11 @@ def test_outofrange_average():
 
     # Test for the beginning is out of range
     with pytest.raises(ValueError):
-        hrm.main('full_test.csv', 250002, avg = True)
+        hrm.main('full_test.csv', 250, avg = True)
 
     # Test for ending out of range
     with pytest.raises(ValueError):
-        hrm.main('full_test.csv', 0, 2500002, avg=True)
+        hrm.main('full_test.csv', 0, 250, avg=True)
 
     # Test for if begin time is after end time
     with pytest.raises(ValueError):
