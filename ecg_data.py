c2b710542b4495ff37eabc182eece16fca4c77e1
syiyuli <samuel.li@duke.edu>
2017-10-03 15:30:34 -0400

    added class file

---
+++ ecg_data.py
@@ -0,0 +1,12 @@
+class hrm:
+    'This is a class'
+    def __init__(self, filename, avgT1,avgT2,bradyT,bradyThresh,tachyT,tachyThresh):
+        self.avgT1 = 0.0
+        self.avgT2 = 10.0
+
+    def instantHr(self):
+
+    def averageHr(self):
+
+    def anomalyHr(self):
+
