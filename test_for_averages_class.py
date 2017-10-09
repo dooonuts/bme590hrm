976017cfcb0a4e9807cbc31875e0e7712d34a17b
cshackerpro <brianna.loomis@duke.edu>
2017-10-09 19:03:12 -0400

    Edited new tests to fit with the class based

--- test_for_averages_class.py
+++ test_for_averages_class.py
@@ -8,26 +8,26 @@ def test_for_average_class():
 
     """
 
-    myObject = Ecg_data('full_test.csv')
+    myHrm = ecg_data.Ecg_data('full_test.csv')
 
-    assert round(myObject.averageHr('full_test.csv',avg = True), 0) == 78;
+    assert round(myHrm.averageHr(), 0) == 78;
 
-    assert round(myObject.averageHr('full_test.csv',30,45,avg=True),0)==42;
+    assert round(myHrm.averageHr(20, 45),0)==42;
 
 def test_outofrange_average_class():
     """Unit test for checking ValueErrors
     """
     # test for start time out of range
     with pytest.raises(ValueError):
-        myObject.averageHr('full_test.csv',250,avg=True)
+        myHrm.averageHr(250)
 
     # test for end time out of range 
     with pytest.raises(ValueError):
-        myObject.averageHr('full_test.csv',0,250,avg=True)
+        myHrm.averageHr(0, 250)
 
     #test for start time after end time
     with pytest.raises(ValueError):
-        myObject.averageHr('full_test.csv',300,250,avg=True)
+        myHrm.averageHr(300, 250)
 
 
 
